9e9957ffce2bf612bec680f3d493a0fa23e3f895
Alexander Solovyov <alexander.solovyov@umbrellait.com>
2025-10-27 20:57:11 +0300

    init

--- src/shared/ui/tooltip/tooltip.js
+++ src/shared/ui/tooltip/tooltip.js
@@ -6,7 +6,18 @@ export class TooltipComponent {
     this.hideTimeout = null;
 
     const range = document.querySelector('.custom-range');
-    const input = document.querySelector('.custom-input');
+    this.input = document.querySelector('.custom-input');
+
+    this.calculatedPoint = document.querySelector('.points');
+    this.star = document.querySelector('.star-icon');
+
+    const group = document.getElementById('radio-group');
+
+    group.addEventListener('change', (e) => {
+      if (e.target.matches('input[name="configuration"]')) {
+        this.recalc();
+      }
+    });
 
     function updateGradient() {
       const value = ((range.value - range.min) / (range.max - range.min)) * 100;
@@ -27,24 +38,29 @@ export class TooltipComponent {
 
     // при движении ползунка обновляем текстовое поле
     range.addEventListener('input', () => {
-      input.value = range.value;
+      this.input.value = range.value;
+      this.recalc();
     });
 
     // при вводе текста обновляем ползунок (в пределах 0–300)
-    input.addEventListener('input', () => {
-      let value = parseInt(input.value, 10);
+    this.input.addEventListener('input', () => {
+      let value = parseInt(this.input.value, 10);
 
       if (isNaN(value)) value = 0;
       if (value < 0) value = 0;
       if (value > 300) value = 300;
 
-      input.value = value;
+      this.input.value = value;
       range.value = value;
 
       const sliderValue =
         ((range.value - range.min) / (range.max - range.min)) * 100;
       range.style.setProperty('--value', `${sliderValue}%`);
+
+      this.recalc();
     });
+
+    this.recalc();
   }
 
   show(target, data) {
@@ -75,5 +91,36 @@ export class TooltipComponent {
     const top = rect.top + window.scrollY - ttRect.height - 8;
     this.el.style.left = `${left}px`;
     this.el.style.top = `${top}px`;
+  }
+
+  recalc() {
+    const inp = parseFloat(this.input.value) || 0;
+
+    const selectedRadio = document.querySelector(
+      'input[name="configuration"]:checked'
+    );
+
+    let result = 0;
+    const base = inp * 3;
+
+    switch (selectedRadio?.value) {
+      case 'standard':
+        result = base;
+        break;
+      case 'elite':
+        result = base + Math.ceil(base * 0.1);
+        break;
+      case 'premium':
+        result = base + Math.ceil(base * 0.2);
+        break;
+    }
+
+    this.calculatedPoint.textContent = `${result}`;
+    this.calculatedPoint.classList.remove('pulse');
+    this.star.classList.remove('pulse');
+    void this.calculatedPoint.offsetWidth; // перезапускаем анимацию
+    void this.star.offsetWidth; // перезапускаем анимацию
+    this.calculatedPoint.classList.add('pulse');
+    this.star.classList.remove('pulse');
   }
 }
