cef9988ce7dae3778145b8e0d477bda2f2a022d4
Alexander Solovyov <alexander.solovyov@umbrellait.com>
2025-10-28 20:17:56 +0300

    init

--- src/todo.js
+++ src/todo.js
@@ -1,14 +1,4 @@
-// TODO: check SEO
 // TODO: check img loading with slow connection
 // TODO: add font to files
 // TODO: check png to svg
 // TODO: delete commented code!
-// TODO:
-// TODO: (optional) - variant with tooltip lib
-// TODO: (optional) - move to TS
-// TODO: (optional) - minification js
-// TODO:
-// TODO:
-// TODO:
-// TODO:
-// TODO:
